1da6bd0432be04286be6493818b3ea2247da7b72
DanhPham <danhpham1803@gmail.com>
2025-10-25 20:39:35 +0700

    feat: add session 2

---
+++ scripts/session_2/train.py
@@ -0,0 +1,12 @@
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s - %(message)s",
+)
+logger = logging.getLogger("housing")
+
+
+def train():
+    pass
