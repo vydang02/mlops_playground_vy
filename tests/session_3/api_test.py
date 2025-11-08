b86bfa90b5e92e7c760f63d77147071677c68f30
DanhPham <danhpham1803@gmail.com>
2025-11-08 21:42:13 +0700

    feat: add tests and workflow to run them

--- tests/session_3/api_test.py
+++ tests/session_3/api_test.py
@@ -1,52 +1,12 @@
-import pytest
+# ..._test.py
 from fastapi.testclient import TestClient
 
 from scripts.session_3.api import app
 
-
-@pytest.fixture
-def client():
-    return TestClient(app)
+client = TestClient(app)
 
 
-def test_root(client):
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, FastAPI!"}
-
-
-def test_calculate_add(client):
-    response = client.post("/calculate", json={"method": "add", "num1": 1, "num2": 2})
-    assert response.status_code == 200
-    assert response.json() == {"result": 3}
-
-
-def test_calculate_subtract(client):
-    response = client.post(
-        "/calculate", json={"method": "subtract", "num1": 1, "num2": 2}
-    )
-    assert response.status_code == 200
-    assert response.json() == {"result": -1}
-
-
-def test_calculate_multiply(client):
-    response = client.post(
-        "/calculate", json={"method": "multiply", "num1": 1, "num2": 2}
-    )
-    assert response.status_code == 200
-    assert response.json() == {"result": 2}
-
-
-def test_calculate_divide(client):
-    response = client.post(
-        "/calculate", json={"method": "divide", "num1": 1, "num2": 2}
-    )
-    assert response.status_code == 200
-    assert response.json() == {"result": 0.5}
-
-
-def test_calculate_invalid_method(client):
-    response = client.post(
-        "/calculate", json={"method": "invalid", "num1": 1, "num2": 2}
-    )
-    assert response.status_code == 422
